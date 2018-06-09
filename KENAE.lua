1c0b8fe9b324eed54354a814021c5fe6f6bad6af
كيناي <32018768+KENAEM@users.noreply.github.com>
2018-06-09 13:03:51 -0700

    Update KENAE.lua

--- KENAE.lua
+++ KENAE.lua
@@ -4457,7 +4457,7 @@ if (text and text == 'تغير امر م1') and tonumber(msg.sender_user_id_) ==
 @KEN_AE     المطور
 @KE_NAE  قنا ة السورس  
 
-]]
+  ]]
  send(msg.chat_id_, msg.id_, 1, text, 1, 'id')
   end
 
