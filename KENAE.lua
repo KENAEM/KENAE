63bb6cd07abc3b202d95c8cd8392e78b78f53cc0
كيناي <32018768+KENAEM@users.noreply.github.com>
2018-06-10 09:52:23 -0700

    Update KENAE.lua

--- KENAE.lua
+++ KENAE.lua
@@ -4460,6 +4460,7 @@ if (text and text == 'تغير امر م1') and tonumber(msg.sender_user_id_) ==
 ]]
 send(msg.chat_id_, msg.id_, 1, text, 1, 'html')
  end
+
 if text:match("^اريد رابط حذف$") or text:match("^رابط حذف$") or text:match("^رابط الحذف$") or text:match("^الرابط حذف$") or text:match("^اريد رابط الحذف$") then
 local text =  [[
 🗑┇رابط حذف التلي ، ⬇
