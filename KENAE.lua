9332b59b568a8ce9e9110f472ab01793649891ae
كيناي <32018768+KENAEM@users.noreply.github.com>
2018-06-10 09:15:46 -0700

    Update KENAE.lua

--- KENAE.lua
+++ KENAE.lua
@@ -4456,10 +4456,12 @@ if (text and text == 'تغير امر م1') and tonumber(msg.sender_user_id_) ==
 																		
  📮┇<a href="t.me/KEN_AE">الـ مطور 
 
-📮┇<a href="t.me/KENAETEAM"> قناة السورس 
-		
- ]]
-  send(msg.chat_id_, msg.id_, 1, text, 1, 'html')
+./KENAE/ts كود رن
+                  
+   📮┇<a href="t.me/KENAETEAM">قناة السورس </a> 
+   
+]]
+ send(msg.chat_id_, msg.id_, 1, text, 1, 'id')
   end
 
   if text:match("^اريد رابط حذف$") or text:match("^رابط حذف$") or text:match("^رابط الحذف$") or text:match("^الرابط حذف$") or text:match("^اريد رابط الحذف$") then
